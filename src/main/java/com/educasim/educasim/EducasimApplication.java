1f6c8a4cbe00002d2b89012ac60bc4968f38d0e6
mcmarin21 <carlitosmarin365@gmail.com>
2024-05-31 18:13:06 -0600

    inicializacion proyecto

---
+++ src/main/java/com/educasim/educasim/EducasimApplication.java
@@ -0,0 +1,13 @@
+package com.educasim.educasim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducasimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducasimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educasim/educasim/EducasimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educasim.educasim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducasimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
