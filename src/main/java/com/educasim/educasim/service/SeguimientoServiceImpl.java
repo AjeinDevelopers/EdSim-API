8f52d6dc6c2a4afe76f74a2d5c768a6457c42f5d
mcmarin21 <carlitosmarin365@gmail.com>
2024-06-03 23:31:06 -0600

    HashMap

--- src/main/java/com/educasim/educasim/service/SeguimientoServiceImpl.java
+++ src/main/java/com/educasim/educasim/service/SeguimientoServiceImpl.java
@@ -3,6 +3,7 @@ package com.educasim.educasim.service;
 import com.educasim.educasim.domain.Progreso;
 import com.educasim.educasim.domain.Usuario;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SeguimientoServiceImpl implements SeguimientoService{
