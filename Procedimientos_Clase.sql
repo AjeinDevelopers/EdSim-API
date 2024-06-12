da502398aea930649d86ca95ce6e2a1dbf51c127
mcmarin21 <carlitosmarin365@gmail.com>
2024-06-12 00:36:41 -0600

    Procedimientos clase

--- Procedimientos_Clase.sql
+++ Procedimientos_Clase.sql
@@ -91,3 +91,116 @@ BEGIN
 END;//
 
 delimiter ;
+
+drop procedure if exists sp_listarAlumnos;
+
+delimiter //
+
+create procedure sp_listarAlumnos(
+	
+    in p_idClase int,
+    in profesor varchar(6)
+    
+)
+BEGIN
+
+	DECLARE p_idProfe int;
+    DECLARE existe int;
+    DECLARE existeProfe int;
+    DECLARE Xmsg varchar(50);
+    
+    set existeProfe = (select count(*) from relSesionProf where idSesion = p_sesion);
+    if existeProfe = 1 then
+		set p_idProfe = (select idProfesor from relSesionProf where idSesion = p_sesion);
+        set existe = (select count(*) from Clase where idClase = p_idClase and idProfesor = p_idProfe);
+        if existe = 1 then
+			select Alumno.idAlumno, Alumno.nombreAlum, Alumno.apePatAlum, Alumno.apeMatAlum from RelAlumClase
+            inner join Alumno
+            on RelAlumClase.idAlumno = Alumno.idAlumno
+            where RelAlumClase.idClase = p_idClase;
+        else
+			set Xmsg = "error";
+            select Xmsg;
+        end if;
+    else
+		set Xmsg = "error";
+        select Xmsg;
+    end if;
+
+END;//
+
+delimiter ;
+
+drop procedure if exists sp_claseAddAlumno;
+
+delimiter //
+
+create procedure sp_claseAddAlumno(
+	
+    IN p_alumno varchar(100),
+    IN p_idClase varchar(6)
+    
+)
+BEGIN
+
+	DECLARE p_idAlumno int;
+    DECLARE existeAlumno int;
+    DECLARE existeClase int;
+    DECLARE Xmsg varchar(50);
+    
+    set existeAlumno = (select count(*) from relSesionAlum where idSesion = p_alumno);
+    set existeClase = (select count(*) from Clase where idClase = p_idClase);
+    if existeAlumno = 1 and existeClase = 1 then
+		set p_idAlumno = (select idAlumno from relSesionAlum where idSesion = p_alumno);
+        insert into RelAlumClase(idAlumno, idClase) values(p_idAlumno, p_idClase);
+        set Xmsg = "inserto";
+        select Xmsg;
+	else
+		set Xmsg = "error";
+        select Xmsg;
+    end if;
+
+END;//
+
+delimiter ;
+
+drop procedure if exists sp_claseDeleteAlumno;
+
+delimiter //
+
+create procedure sp_claseDeleteAlumno(
+	
+    IN p_idAlumno int(3),
+    IN p_idClase int(3)
+    
+)
+BEGIN
+
+    DECLARE existeAlumno int;
+    DECLARE existeClase int;
+    DECLARE Xmsg varchar(50);
+    
+    set existeAlumno = (select count(*) from Alumno where idAlumno = p_idAlumno);
+    set existeClase = (select count(*) from Clase where idClase = p_idClase);
+    if existeAlumno = 1 and existeClase = 1 then
+        delete from RelAlumClase where idAlumno = p_idAlumno and idClase = p_idClase;
+        set Xmsg = "borradito";
+        select Xmsg;
+	else
+		set Xmsg = "error";
+        select Xmsg;
+    end if;
+
+END;//
+
+delimiter ;
+
+drop view if exists vw_obtenerMaterias;
+
+create view vw_obtenerMaterias as
+select * from Materia;
+
+drop view if exists vw_obtenerFases;
+
+create view vw_obtenerFases as
+select * from fase;
